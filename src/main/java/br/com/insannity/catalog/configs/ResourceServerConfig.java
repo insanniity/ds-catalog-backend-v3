b950599f2c83e5597d816cb4506638a18e999ad2
Cleydson <cleydson.cg@live.com>
2022-07-14 22:32:20 -0300

    Resource Server

---
+++ src/main/java/br/com/insannity/catalog/configs/ResourceServerConfig.java
@@ -0,0 +1,78 @@
+package br.com.insannity.catalog.configs;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableResourceServer
+@RequiredArgsConstructor
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    private final Environment environment;
+    private final JwtTokenStore tokenStore;
+
+    private static final String[] PUBLIC = {"/oauth/**", "/oauth/authorize/**", "/actuator/**", "/h2/**"};
+
+    private static final String[] ADMIN = {"/users/**"};
+    private static final String[] OPERADOR = {"/products/**", "/categories/**"};
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        // H2-CONSOLE
+//        if (Arrays.asList(environment.getActiveProfiles()).contains("dev")) {
+//        }
+        http.headers().frameOptions().disable();
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET ,OPERADOR).permitAll()
+                .antMatchers(ADMIN).hasAnyRole("ADMIN")
+                .antMatchers(OPERADOR).hasAnyRole("ADMIN", "OPERATOR")
+                .anyRequest().authenticated();
+
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(List.of("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+                new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
